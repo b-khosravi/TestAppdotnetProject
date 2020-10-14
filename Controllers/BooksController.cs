73d012a9296beb1eabeeb36749ea8e66f4fe62c7
boshra khosravi <boshra68_kh@yahoo.com>
2020-10-14 12:09:27 +0330

    change book controller post auth policy

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -26,7 +26,7 @@ namespace TestApp.Controllers
 
         // GET: api/Books/GetBooks
         [HttpGet]
-        [Authorize(Policy="UserLoginOnly")]
+        [Authorize(Policy="RequireUserRole")]
         public async Task<ActionResult<IEnumerable<Book>>> GetBooks(int pageNumber=1,int pageSize=3)
         {
 
@@ -96,7 +96,7 @@ namespace TestApp.Controllers
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
 
-                [HttpPost]
+        [HttpPost]
 
         [Authorize(Policy="ReuireUserRole")]
 
